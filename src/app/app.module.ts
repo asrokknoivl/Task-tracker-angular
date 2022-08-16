28339c45bf8a86c122f59d51d0e5f2e870dc4c81
aioknokovodrisrln <mqaisahm@gmail.com>
2022-08-16 10:48:18 +0200

    git init but at this point I've already added some code but yeah whatever its just some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
